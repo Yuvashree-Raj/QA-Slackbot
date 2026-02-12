df7384bf313e34e77ac09f42b24154c9ab7aa4f8
Yuvashree-Raj <yuvashreerajkumar2025@gmail.com>
2026-02-12 18:40:38 +0530

    Update views.py

--- slackbot/views.py
+++ slackbot/views.py
@@ -1,25 +1,39 @@
 
+import requests
+import threading
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .utils import generate_testcases
 
 
+
+def process_ai(feature_description, response_url):
+    ai_output = generate_testcases(feature_description)
+
+    requests.post(response_url, json={
+        "response_type": "in_channel",
+        "text": ai_output
+    })
+
+
 @csrf_exempt
 def slack_generate_testcases(request):
     if request.method == "POST":
+
         feature_description = request.POST.get("text")
+        response_url = request.POST.get("response_url")
 
-        if not feature_description:
-            return JsonResponse({
-                "response_type": "ephemeral",
-                "text": "Please provide a feature description."
-            })
+        # Start background thread
+        thread = threading.Thread(
+            target=process_ai,
+            args=(feature_description, response_url)
+        )
+        thread.start()
 
-        ai_output = generate_testcases(feature_description)
-
+        # Immediate response
         return JsonResponse({
-            "response_type": "in_channel",
-            "text": ai_output
+            "response_type": "ephemeral",
+            "text": "Generating testcases... please wait ⏳"
         })
 
-    return JsonResponse({"text": "Invalid request method"})
+    return JsonResponse({"error": "Invalid request"})
